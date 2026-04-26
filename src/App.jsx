6dd779fe2557676fc9214a75728e3eed446130b0
jeytla <tiaboujerry5@gmail.com>
2026-04-25 21:49:50 -0400

    Added .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-﻿
+
 import { useRef, useState, useEffect } from 'react'
 import { cn } from './lib/utils'
 import { motion, AnimatePresence, useInView, useScroll, useTransform } from 'framer-motion'
